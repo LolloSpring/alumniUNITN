686a23ab93ad27034aabb812ccb911e860d6cbb8
Lorenzo Tait <lorenzotait@mbpdilorenzo2.home>
2018-08-11 21:39:07 +0200

    add basic folder structure for landing page

---
+++ app.js
@@ -0,0 +1,26 @@
+var express         = require("express"),
+     app            = express(),
+     bodyParser     = require("body-parser"),
+     mongoose       = require("mongoose"),
+     flash          = require("connect-flash"),
+     //seedDB         = require("./seeds"),
+     passport       = require("passport"),
+     LocalStrategy  = require("passport-local"),
+     methodOverride = require("method-override")
+
+
+app.set("view engine", "ejs");
+
+var authRoutes = require("./routes/index");
+
+/*
+app.get('/', function (req, res) {
+  res.render('landing');
+});
+*/
+
+app.use(authRoutes);
+
+app.listen(3000, function () {
+  console.log('alumniUNITN listening on port 3000!');
+});
